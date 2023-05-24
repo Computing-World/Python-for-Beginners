c7692d7e5c6906d1b3ce75e8a91f9425409bd8f5
Computing-World <134471785+Computing-World@users.noreply.github.com>
2023-05-24 18:39:26 +0530

    Create Variables_DataTypes.py

---
+++ Variables_DataTypes/Variables_DataTypes.py
@@ -0,0 +1,23 @@
+
+# defining a variable
+my_variable = 10
+
+# assigning integral values to variables
+my_integer = 2
+print(type(my_integer))
+
+# assigning floating values to variables
+my_float = 10.0
+print(type(my_float))
+
+# assigning text values to variables
+
+my_string = 'Hello World!' # both '' and "" are valid
+print(type(my_string))
+
+# assigning boolean values to variables
+is_python_fun = True
+print(type(is_python_fun))
+
+is_learning_easy = False # True, TRUE and true are different
+print(type(is_learning_easy))
